74e92ce80500b9a028c3b8e2002789ff46e9e42e
Jonathan Vela Silva <jeffthegamerpvp@gmail.com>
2024-03-12 21:12:37 +0100

    finish de metodos para usuario

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,10 +6,9 @@ use App\Models\User;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
-use Inertia\Inertia;
-use Illuminate\Auth\Events\Registered;
+
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class UsersController extends Controller
 {
@@ -19,23 +18,69 @@ class UsersController extends Controller
         $user=User::all();
         return($user);
     }
+    public function show($id)
+    {
+        $user = User::find($id);
+        if($user){
+            return response()->json(['message' => 'Usuario ecncontrado: ',$user], 200);
+        }
+    
+        return response()->json(['message'=>'usuario no encontrado'], 404);
+    }
     public function store(Request $request)
     {
-        $request->validate([
-            'name'=>'max:100|string',
-            'email'=>'max:255|string|email|',
-            'password'=>'max:100|string',
+        $validator = Validator::make($request->all(), [
+            'name'=>'required|max:100|string',
+            'email'=>'required|max:255|string|email|unique:'.User::class,
+            'password'=>'required|max:100|string',
             'isActive'=>'max:100|string',
             'role_id'=>''
         ]);
+    
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
   
-        User::create([
+       $user= User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' =>$request->password,
-            'isActive' =>$request->isActive,
+            'password' => Hash::make($request->password),
+            'isActive'=>$request->isActive,
             'role_id' => $request->role_id
         ]);
-        return($request);
+        return response()->json($user, 201);
+
     }
+    public function update(Request $request, $id)
+{
+    $validator = Validator::make($request->all(), [
+        'name'=>'required|max:100|string',
+        'email'=>'required|max:255|string|email|unique:'.User::class,
+        'password'=>'max:100|string',
+        'isActive'=>'max:100|string',
+        'role_id'=>''
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json($validator->errors(), 400);
+    }
+
+    $user = User::find($id);
+    $user->update($request->all());
+
+    return response()->json($user, 200);
+}
+
+public function destroy($id)
+{
+    $user = User::find($id);
+
+    if($user){
+        $user->delete();
+        return response()->json(['message' => 'Usuario eliminado: ',$user], 200);
+    }
+
+    return response()->json(['message'=>'usuario no encontrado'], 404);
+}
+
 }
